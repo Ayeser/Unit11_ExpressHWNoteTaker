5422f350806c4c41869ec3d186dd5d14196c5d56
Matthew Wester <MatthewWester@live.com>
2020-04-07 18:04:12 -0700

    took out css and js calls out of server file

--- server.js
+++ server.js
@@ -22,14 +22,6 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-app.get("/public/assets/css/styles.css", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/assets/css/styles.css"));
-});
-
-app.get("/public/assets/js/index.js", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/assets/js/index.js"));
-});
-
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
